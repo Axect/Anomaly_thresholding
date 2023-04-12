de4995210ff244205d26a4e8d66f0287f61b0f0d
axect <axect@outlook.kr>
2023-04-12 11:51:03 +0900

    Anomaly Thresholding

---
+++ pq_plot.py
@@ -0,0 +1,153 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import scienceplots
+
+# Import netCDF file
+df = pd.read_parquet("./test.parquet")
+
+# Prepare Data to Plot
+x = df['x']
+y = df['y']
+y_hat = df['y_test']
+y_up = df['y_up']
+y_down = df['y_down']
+l1 = df['l1']
+l1_signed = df['l1_s']
+psqi = df['psqi']
+psqi_t = df['psqi_t']
+psqi_s = df['psqi_s']
+psqi_st = df['psqi_st']
+ub = np.array(df['ub'])
+ubt = np.array(df['ub_t'])
+ubs = np.array(df['ub_s'])
+ubst = np.array(df['ub_st'])
+
+ub_vec = np.array([ub, ubt, ubs, ubst])
+
+# Plot params
+pparam = dict(
+    xlabel = r'$x$',
+    ylabel = r'$y$',
+    xscale = 'linear',
+    yscale = 'linear',
+    ylim = (0, ub_vec.max() + 0.1),
+)
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, l1, label=r'$L_1$')
+    ax.plot(x, ub, 'r-', label=r'Upper Bound')
+    ax.plot(x, ubt, 'g--', label=r'Upper Bound Trimmed')
+    ax.plot(x, ubs, 'b-.', label=r'Upper Bound Signed')
+    ax.plot(x, ubst, 'k:', label=r'Upper Bound Signed Trimmed')
+    ax.legend()
+    fig.savefig('l1_plot.png', dpi=300, bbox_inches='tight')
+
+# Plot params
+pparam = dict(
+    xlabel = r'$x$',
+    ylabel = r'$y$',
+    xscale = 'linear',
+    yscale = 'linear',
+    ylim = (-ub_vec.max() - 0.1, ub_vec.max() + 0.1),
+)
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, l1_signed, label=r'$L_1$ Signed')
+    ax.plot(x, ub, 'r-', label=r'Upper Bound')
+    ax.plot(x, -ub, 'r-')
+    ax.plot(x, ubt, 'g--', label=r'Upper Bound Trimmed')
+    ax.plot(x, -ubt, 'g--')
+    ax.plot(x, ubs, 'b-.', label=r'Upper Bound Signed')
+    ax.plot(x, -ubs, 'b-.')
+    ax.plot(x, ubst, 'k:', label=r'Upper Bound Signed Trimmed')
+    ax.plot(x, -ubst, 'k:')
+    ax.legend()
+    fig.savefig('l1_signed_plot.png', dpi=300, bbox_inches='tight')
+
+ics = np.arange(len(psqi))
+ano = ics[~psqi]
+ano_t = ics[~psqi_t]
+ano_s = ics[~psqi_s]
+ano_st = ics[~psqi_st]
+
+# Plot params
+pparam = dict(
+    xlabel = r'$x$',
+    ylabel = r'$y$',
+    xscale = 'linear',
+    yscale = 'linear',
+    ylim = (y_down.min(), y_up.max()),
+)
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, y_hat, '.', color='gray', markersize=0.5, label=r'$\hat{y}$')
+    ax.plot(x[ano], y_hat[ano],         'r.', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano_t], y_hat[ano_t],     '.', label=r'$\hat{y}_t$')
+    #ax.plot(x[ano_s], y_hat[ano_s],     '.', label=r'$\hat{y}_s$')
+    #ax.plot(x[ano_st], y_hat[ano_st],   '.', label=r'$\hat{y}_{st}$')
+    ax.plot(x, y, 'k-', label=r'$y$')
+    ax.plot(x, y_up, 'k--', label=r'$y_{up}$')
+    ax.plot(x, y_down, 'k--', label=r'$y_{down}$')
+    ax.legend()
+    fig.savefig('ano_plot.png', dpi=300, bbox_inches='tight')
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, y_hat, '.', color='gray', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano], y_hat[ano],         'r.', markersize=0.5, label=r'$\hat{y}$')
+    ax.plot(x[ano_t], y_hat[ano_t],     'g.', markersize=0.5, label=r'$\hat{y}_t$')
+    #ax.plot(x[ano_s], y_hat[ano_s],     'b.', markersize=0.5, label=r'$\hat{y}_s$')
+    #ax.plot(x[ano_st], y_hat[ano_st],   'k.', markersize=0.5, label=r'$\hat{y}_{st}$')
+    ax.plot(x, y, 'k-', label=r'$y$')
+    ax.plot(x, y_up, 'k--', label=r'$y_{up}$')
+    ax.plot(x, y_down, 'k--', label=r'$y_{down}$')
+    ax.legend()
+    fig.savefig('ano_t_plot.png', dpi=300, bbox_inches='tight')
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, y_hat, '.', color='gray', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano], y_hat[ano],         'r.', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano_t], y_hat[ano_t],     'g.', markersize=0.5, label=r'$\hat{y}_t$')
+    ax.plot(x[ano_s], y_hat[ano_s],     'b.', markersize=0.5, label=r'$\hat{y}_s$')
+    #ax.plot(x[ano_st], y_hat[ano_st],   'k.', markersize=0.5, label=r'$\hat{y}_{st}$')
+    ax.plot(x, y, 'k-', label=r'$y$')
+    ax.plot(x, y_up, 'k--', label=r'$y_{up}$')
+    ax.plot(x, y_down, 'k--', label=r'$y_{down}$')
+    ax.legend()
+    fig.savefig('ano_s_plot.png', dpi=300, bbox_inches='tight')
+
+# Plot
+with plt.style.context(["science", "nature"]):
+    fig, ax = plt.subplots()
+    ax.autoscale(tight=True)
+    ax.set(**pparam)
+    ax.plot(x, y_hat, '.', color='gray', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano], y_hat[ano],         'r.', markersize=0.5, label=r'$\hat{y}$')
+    #ax.plot(x[ano_t], y_hat[ano_t],     'g.', markersize=0.5, label=r'$\hat{y}_t$')
+    #ax.plot(x[ano_s], y_hat[ano_s],     'b.', markersize=0.5, label=r'$\hat{y}_s$')
+    ax.plot(x[ano_st], y_hat[ano_st], '.', color='purple', markersize=0.5, label=r'$\hat{y}_{st}$')
+    ax.plot(x, y, 'k-', label=r'$y$')
+    ax.plot(x, y_up, 'k--', label=r'$y_{up}$')
+    ax.plot(x, y_down, 'k--', label=r'$y_{down}$')
+    ax.legend()
+    fig.savefig('ano_st_plot.png', dpi=300, bbox_inches='tight')
---
+++ src/main.rs
@@ -0,0 +1,87 @@
+use peroxide::fuga::*;
+
+const N: usize = 10000;
+const M: usize = 20;
+const MTEST: usize = 10000;
+
+fn main() {
+    let n = Normal(0.0, 0.1);
+    let mut eps = n.sample(N);
+    let mut ics = (0usize .. N).collect::<Vec<_>>();
+    ics.shuffle(&mut thread_rng());
+    let ics_picked = ics[0 .. M].to_vec();
+    for i in ics_picked {
+        eps[i] *= 3.0;
+    }
+
+    let mut eps_test = n.sample(N);
+    let mut ics_test = (0usize .. N).collect::<Vec<_>>();
+    ics_test.shuffle(&mut thread_rng());
+    let ics_picked_test = ics_test[0 .. MTEST].to_vec();
+    for i in ics_picked_test {
+        eps_test[i] *= 10.0;
+    }
+
+    let x = linspace(0, std::f64::consts::PI, N);
+    let y = x.fmap(|t| t.sin());
+    let y_hat = zip_with(|x, e| x + e, &y, &eps);
+    let y_test = zip_with(|x, e| x + e, &y, &eps_test);
+    let y_up = y.fmap(|y| y + 3.0 * 0.1);
+    let y_down = y.fmap(|y| y - 3.0 * 0.1);
+
+    let l1 = zip_with(|y, y_hat| (y - y_hat).abs(), &y, &y_hat);
+    let l1_test = zip_with(|y, y_hat| (y - y_hat).abs(), &y, &y_test);
+
+    // Ordinary 3-sigma rule
+    let mean = l1.mean();
+    let std = l1.sd();
+    let upper_bound = mean + 3.0 * std;
+    let psqi = l1_test.iter().map(|&x| x < upper_bound).collect::<Vec<_>>();
+
+    // Trimmed 3-sigma rule
+    let (mean, std) = iqr_trimmed_mean_std(&l1);
+    let upper_bound_trimmed = mean + 3.0 * std;
+    let psqi_trimmed = l1_test.iter().map(|&x| x < upper_bound_trimmed).collect::<Vec<_>>();
+
+    // Not absolute 3-sigma rule
+    let l1_signed = zip_with(|y, y_hat| y_hat - y, &y, &y_hat);
+    let mean = l1_signed.mean();
+    let std = l1_signed.sd();
+    let upper_bound_signed = mean + 3.0 * std;
+    let psqi_signed = l1_test.iter().map(|&x| x < upper_bound_signed).collect::<Vec<_>>();
+
+    // Not absolute trimmed 3-sigma rule
+    let (mean, std) = iqr_trimmed_mean_std(&l1_signed);
+    let upper_bound_signed_trimmed = mean + 3.0 * std;
+    let psqi_signed_trimmed = l1_test.iter().map(|&x| x < upper_bound_signed_trimmed).collect::<Vec<_>>();
+
+    let mut df = DataFrame::new(vec![]);
+    df.push("x", Series::new(x));
+    df.push("y", Series::new(y));
+    df.push("y_hat", Series::new(y_hat));
+    df.push("y_test", Series::new(y_test));
+    df.push("y_up", Series::new(y_up));
+    df.push("y_down", Series::new(y_down));
+    df.push("l1", Series::new(l1));
+    df.push("l1_s", Series::new(l1_signed));
+    df.push("psqi", Series::new(psqi));
+    df.push("psqi_t", Series::new(psqi_trimmed));
+    df.push("psqi_s", Series::new(psqi_signed));
+    df.push("psqi_st", Series::new(psqi_signed_trimmed));
+    df.push("ub", Series::new(vec![upper_bound; N]));
+    df.push("ub_t", Series::new(vec![upper_bound_trimmed; N]));
+    df.push("ub_s", Series::new(vec![upper_bound_signed; N]));
+    df.push("ub_st", Series::new(vec![upper_bound_signed_trimmed; N]));
+
+    df.print();
+
+    df.write_parquet("test.parquet", CompressionOptions::Uncompressed).unwrap();
+}
+
+fn iqr_trimmed_mean_std(x: &[f64]) -> (f64, f64) {
+    let n = x.len();
+    let x = x[n/4 .. 3*n/4].to_vec();
+    let mean = x.mean();
+    let std = x.sd();
+    (mean, std)
+}
